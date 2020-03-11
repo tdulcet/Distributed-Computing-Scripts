8d45d38502fbc0fb319344a20ec4e6e44ee67266
Danc2050 <danc2@pdx.edu>
2020-03-10 17:04:03 -0700

    nearly done with mprime port

--- mprime-python-port/mprime.py
+++ mprime-python-port/mprime.py
@@ -1,19 +1,52 @@
+#!/usr/bin/env python3
+
 #Daniel Connelly
-# Usage: ./mprime.sh [PrimeNet User ID] [Computer name] [Type of work] [Idle time to run]
+# Usage: ./mprime.py [PrimeNet User ID] [Computer name] [Type of work] [Idle time to run]
+# wget https://raw.github.com/tdulcet/Distributed-Computing-Scripts/master/mprime.sh -qO - | bash -s --
+# ./mprime.py [PrimeNet User ID] [Computer name] [Type of work] [Idle time to run]
+# ./mprime.py "$USER" "$HOSTNAME" 100 10
+# ./mprime.py ANONYMOUS
 
 import sys
 import subprocess
-import os
+from os import environ, path, chdir
 import signal
 import re # regular expression matching
-#print(sys.argv[0])
-#sys.exit()
+from hashlib import sha256 # verifying we downloaded the correct mprime tar.gz
+import hashlib
 
-os.environ["USERID"], os.environ["COMPUTER"], os.environ["TYPE"]  = sys.argv[1], sys.argv[2], sys.argv[3]
+def regex_check(reg, var, err):
+    if not re.match(reg, var):
+        sys.stderr.write(err)
+        sys.exit(1)
 
-#---Checks and Dependencies/Downloads---#
+def misc_check(condition, err):
+    if condition:
+        sys.stderr.write(err)
+        sys.exit(1)
+
+# Source: https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+# There is no sha256sum in the hashlib library normally
+def sha256sum(filename):
+    h  = hashlib.sha256()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+    with open(filename, 'rb', buffering=0) as f:
+        for n in iter(lambda : f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+environ["DIR"] = "prime95" # TODO -- change to fit Teal's mprime filename
+environ["FILE"] = "p95v298b3.linux64.tar.gz"
+environ["SUM"] = "66117E8C30752426471C7B4A7A64FFBFC53C84D0F3140ACF87C08D3FEC8E99AC"
+if len(sys.argv) == 5:
+    environ["TIME"] = sys.argv[4]
+    regex_check(r'^([0-9]*[.])?[0-9]+$', environ["TIME"], "Usage: [Type of work] is not a valid number")
+environ["USERID"], environ["COMPUTER"], environ["TYPE"]  = sys.argv[1], sys.argv[2], sys.argv[3]
+
+#---Dependencies/Downloads---#
 print("Asserting Python version is >= Python3.6")
-assert sys.version_info >= (3, 6) #Pyhon 3 required
+assert sys.version_info >= (3, 6)
 
 try:
     import wget
@@ -21,49 +54,57 @@ except ImportError as error:
     print("Installing wget dependency")
     p = subprocess.run('pip install wget', shell=True)
     import wget
+except Exception as error:
+    misc_check(true, "Unexpected error occured when installing Python dependency:\n\n" + error)
+#----------------------------#
 
-#TODO -- Regex
-RE='^([024568]|1(0[0124]|5[0124]|6[01])?)$'
-#if not os.environ["TYPE"] ==
+#---Command Line Checks------#
+misc_check(len(sys.argv) > 4, "Usage: " + sys.argv[0]+ " [PrimeNet User ID] [Computer name] [Type of work] [Idle time to run]")
+regex_check(r'^([024568]|1(0[0124]|5[0124]|6[01])?)$', environ["TYPE"], "Usage: [Type of work] is not a valid number")
+#----------------------------#
 
-#---------------------------------------#
+#---File/Folder checks-------#
+#misc_check(path.exists(environ["DIR"]), "Error: Prime95 is already downloaded")
+misc_check(sha256sum("./prime95/p95v298b3.linux64.tar.gz") == environ["SUM"], "Error: Prime95 is already downloaded")
+#----------------------------#
+
 #---Directory/unzipping---#
 #print("Making directory to house contents of prime95")
 #os.mkdir("prime95")
+#misc_check(not os.path.exists(DIR), "Error: Failed to create directory " + environ["DIR"])
 
 print("Changing to directory")
-os.chdir("prime95")
+chdir("prime95")
 
-os.environ["FILE"] = "p95v298b3.linux64.tar.gz"
 #print("Downloading prime95...")
-#wget.download('https://www.mersenne.org/ftp_root/gimps/'+os.environ["FILE"])
+#wget.download('https://www.mersenne.org/ftp_root/gimps/'+environ["FILE"])
 
 print("Unzipping folder here...")
-subprocess.run(['tar', '-xzvf', os.environ["FILE"]])
+subprocess.run(['tar', '-xzvf', environ["FILE"]])
 #---------------------------------------#
 
 #---Configuration---#
-args = [os.environ["USERID"], os.environ["COMPUTER"], os.environ["TYPE"]]
-print("Configuring...")
+args = [environ["USERID"], environ["COMPUTER"], environ["TYPE"]]
+print("Setting up Prime95.")
 p = subprocess.Popen(['python3', "../exp.py"] + args,
-#p = subprocess.Popen(['python3', "../exp2.py"] + args,
   stdout=subprocess.PIPE,
   universal_newlines=True,
   bufsize=0)
 
-#p.wait()
-
+# This code block reads the output from the pexpect script
 try:
   for line in p.stdout:
     line=line[:-1]
     print(line)
-except KeyboardInterrupt:
-  print("\nExiting...")
-  os.kill(p, signal.SIGKILL)
-  print("Done.")
 #---------------------------------------#
 
 #---Starting Program---#
+print("Starting up Prime95.")
+subprocess.Popen("./mprime") # daemon process
+print ("Mprime is running in the background...")
+#subprocess.run('crontab -l | { cat; echo "* * * * * if who -s | awk '{ print \$2 }' | (cd /dev && xargs -r stat -c '\%U \%X    ') | awk '{if ('\"\$(date +\%s)\"'-\$2<$TIME) { print \$1\"\t\"'\"\$(date +\%s)\"'-\$2; ++count }} END{if (    count>0) { exit 1 }}' > /dev/null; then pgrep mprime > /dev/null || (cd $DIR && nohup ./mprime &); else pgr    ep mprime > /dev/null && killall mprime; fi"; } | crontab -, shell=True)
+
+''' # this reads the output for my testing purposes
 print("Starting mprime")
 p = subprocess.Popen(['./mprime', '-d'],
   stdout=subprocess.PIPE,
@@ -78,3 +119,9 @@ except KeyboardInterrupt:
   print("\nExiting...")
   os.kill(p, signal.SIGKILL)
   print("Done.")
+'''
+
+'''
+#TODO -- I need some explanation on what this is doing, Teal
+#subprocess.run('crontab -l | { cat; echo "* * * * * if who -s | awk '{ print \$2 }' | (cd /dev && xargs -r stat -c '\%U \%X    ') | awk '{if ('\"\$(date +\%s)\"'-\$2<$TIME) { print \$1\"\t\"'\"\$(date +\%s)\"'-\$2; ++count }} END{if (    count>0) { exit 1 }}' > /dev/null; then pgrep mprime > /dev/null || (cd $DIR && nohup ./mprime &); else pgr    ep mprime > /dev/null && killall mprime; fi"; } | crontab -, shell=True)
+'''
