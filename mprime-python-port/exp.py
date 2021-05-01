ebf4d2bf13b9bb2078bdd4a3c844b27e0907efe2
root <root@SDS-US-72YX.strongboxdata.com>
2021-05-01 14:48:09 -0700

    new

--- mprime-python-port/exp.py
+++ mprime-python-port/exp.py
@@ -15,7 +15,7 @@ try:
     import pexpect
 except ImportError as error:
     print("Installing pexpect...")
-    p = subprocess.run('pip3 install pexpect', shell=True)
+    p = subprocess.run('pip install pexpect', shell=True)
     import pexpect
 
 # Prerequisites, gained from mprime.py
@@ -23,50 +23,36 @@ USERID, COMPUTER, TYPE  = sys.argv[1], sys.argv[2], sys.argv[3]
 
 child = pexpect.spawn('./mprime -m') # starts shell to interact with
 child.logfile = sys.stdout.buffer # enables output to screen (Python 3)
-preThreadExpects = (("Join Gimps?", "y"), ("Use PrimeNet to get work and report results ()", "y"),
-        ("Your user ID or", USERID), ("Optional computer name", COMPUTER))
-        #("Type of work to get", TYPE), ("Your choice:", "5"))
-postThreadExpects =  (("Get occasional proof certification work", ""),
-                      ("Accept the answers above?", ""))
-getThreads = False
-finish = False
+expects = (("Join Gimps?", "y"), ("Use PrimeNet to get work and report results ()", "y"),
+        ("Your user ID or", USERID), ("Optional computer name", COMPUTER),
+        ("Type of work to get", TYPE), ("Your choice:", "5"))
+index = 0
 while 1:
     try:
-        if getThreads == False and finish == False:
-            # handle introductory cases
-            for expect in preThreadExpects:
-                child.expect(expect[0], timeout=2)
-                child.sendline(expect[1])
-            while 1:
-                # case to handle multiple threads without needing to hardcode it
-                try: 
-                    child.expect("Type of work to get", timeout=2)
-                    child.sendline(TYPE)
-                    child.expect("CPU cores to use", timeout=2)
-                    getThreads = True
-                    break
-                except Exception as e:
-                    # will handle all default cases
-                    child.sendline("")
-        elif getThreads and finish == False:
-            while True:
-                try:
-                    child.expect("Type of work to get", timeout=2)
-                    child.sendline(TYPE)
-                    child.expect("CPU cores to use", timeout=2)
-                    child.sendline("")
-                except Exception as e:
-                    finish = True
-                    break
+        if index != len(expects):
+            child.expect(expects[index][0], timeout=1)
+            sleep(1)
+            child.sendline(expects[index][1])
+            index += 1
         else:
-            for expect in postThreadExpects:
-                child.expect(expect[0], timeout=2)
-                child.sendline(expect[1])
-            child.expect("Your choice:")
-            child.sendline("5")
-            child.expect("Choose Test/Continue to restart")
+            child.expect("Done communicating with server.")
+            child.sendline("\x03")
+            sleep(5)
+            child.expect("Choose Test/Continue to restart.")
+            sleep(1)
             child.sendline("5")
             child.expect(pexpect.EOF)
             break
     except pexpect.TIMEOUT:
-        child.sendline("")
+        try: 
+            child.expect("Type of work to get", timeout=1)
+            sleep(1)
+            child.sendline(TYPE)
+            child.expect("CPU cores to use", timeout=5)
+            sleep(1)
+            child.sendline("")
+            break
+        except Exception as e:
+            # will handle all default cases
+            child.sendline("")
+        #child.sendline("")
