d7101b295229afc17b0f08f1bc1ed6961ecc7884
mattsblack <47698245+mattsblack@users.noreply.github.com>
2025-12-10 14:45:22 -0500

    updated folding at home to newest version. also added an account token argument.

--- folding.sh
+++ folding.sh
@@ -2,8 +2,8 @@
 
 # Copyright © 2020 Teal Dulcet
 # wget -qO - https://raw.github.com/tdulcet/Distributed-Computing-Scripts/master/folding.sh | bash -s --
-# ./folding.sh [Username] [Team number] [Passkey] [Power]
-# ./folding.sh "$USER" 0 "" medium
+# ./folding.sh [Username] [Team number] [Passkey] [Power] [AccountToken]
+# ./folding.sh "$USER" 0 "" medium "YOUR_TOKEN_HERE"
 # ./folding.sh anonymous
 
 # sudo dpkg -P fahclient
@@ -11,14 +11,15 @@
 # sudo dpkg -P fahviewer
 
 DIR="folding"
-if [[ $# -gt 4 ]]; then
-	echo "Usage: $0 [Username] [Team number] [Passkey] [Power]" >&2
+if [[ $# -gt 5 ]]; then
+	echo "Usage: $0 [Username] [Team number] [Passkey] [Power] [AccountToken]" >&2
 	exit 1
 fi
 USERID=${1:-$USER}
 TEAM=${2:-0}
 PASSKEY=${3:-}
 POWER=${4:-medium}
+TOKEN=${5:-}
 RE='^[0-9]+$'
 if ! [[ $TEAM =~ $RE ]]; then
 	echo "Usage: [Team number] must be a number" >&2
@@ -38,12 +39,16 @@ echo -e "Username:\t$USERID"
 echo -e "Team number:\t$TEAM"
 echo -e "Passkey:\t$PASSKEY"
 echo -e "Power:\t\t$POWER\n"
+if [[ -z $TOKEN ]]; then
+	echo "AccountToken is not set, using default settings"
+else
+	echo "AccountToken:\t\t$TOKEN"
 if [[ -e idletime.sh ]]; then
 	bash -- idletime.sh
 else
 	wget -qO - https://raw.github.com/tdulcet/Distributed-Computing-Scripts/master/idletime.sh | bash -s
 fi
-if [[ -d $DIR ]] && command -v FAHClient >/dev/null; then
+if [[ -d $DIR ]] && command -v fah-client >/dev/null; then
 	echo "Error: Folding@home is already downloaded and installed" >&2
 	exit 1
 fi
@@ -53,22 +58,38 @@ if ! mkdir "$DIR"; then
 fi
 cd "$DIR"
 echo -e "Downloading Folding@home\n"
-wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.5/fahclient_7.5.1_amd64.deb
-wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.5/fahcontrol_7.5.1-1_all.deb
-wget https://download.foldingathome.org/releases/public/release/fahviewer/debian-stable-64bit/v7.5/fahviewer_7.5.1_amd64.deb
+wget https://download.foldingathome.org/releases/public/fah-client/debian-10-64bit/release/fah-client_8.4.9_amd64.deb
+
 echo -e "\nInstalling Folding@home"
 echo -e "Please enter your password if prompted.\n"
+if ! sudo mkdir -p /etc/fah-client; then
+    echo "Error: Failed to create config directory" >&2
+    exit 1
+fi
 
-# Adapted from: https://www.linuxquestions.org/questions/blog/bittner-195120/howto-automate-interactive-debian-package-installations-debconf-preseeding-2879/
-# dpkg-deb -e fahclient_7.5.1_amd64.deb control_files/
-# grep -e '^Template:' -e '^Type:' -e '^Default:' control_files/templates | xargs | sed -e 's/\s*Template: /\nFAHClient\t/g' -e 's/\s*Type: */\t/g' -e 's/\s*Default: */\t/g' > FAHClient.conf
-sudo debconf-set-selections <<<"FAHClient	fahclient/user	string	$USERID
-FAHClient	fahclient/team	string	$TEAM
-FAHClient	fahclient/passkey	string	$PASSKEY
-FAHClient	fahclient/power	select	$POWER
-FAHClient	fahclient/autostart	boolean	true"
+MACHINE_NAME=$(hostname)
 
-sudo dpkg -i --force-depends fahclient_7.5.1_amd64.deb
-sudo dpkg -i --force-depends fahcontrol_7.5.1-1_all.deb
-sudo dpkg -i --force-depends fahviewer_7.5.1_amd64.deb
+echo -e "Generating configuration file..."
+sudo bash -c "cat > /etc/fah-client/config.xml" <<EOF
+<config>
+  <user v='$USERID'/>
+  <team v='$TEAM'/>
+  <passkey v='$PASSKEY'/>
+  <power v='$POWER'/>
+  <account-token v='$TOKEN'/>
+  <machine-name v='$MACHINE_NAME'/>
+</config>
+EOF
+
+if [[ $? -ne 0 ]]; then
+	echo "Error: Failed to write configuration file" >&2
+	exit 1
+fi
+sudo dpkg -i --force-depends fahclient_8.4.9_amd64.deb
+sudo systemctl restart fah-client
+
+if [[ -z $TOKEN ]]; then
+	echo -e "Account Token is not set, edit /etc/fah-client/config.xml to add your token."
+	echo -e "You can find your token here: https://v8-4.foldingathome.org/account/settings"
+	
 # sudo apt-get install -f
